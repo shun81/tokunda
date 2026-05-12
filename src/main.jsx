32826dbbebb72f2a959d3a9a99710b071a4df948
shun81 <shun.ishii@gmail.com>
2026-05-12 14:58:09 +0900

    Fix Vercel Analytics import - use Analytics component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { SpeedInsights } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    <SpeedInsights />
+    <Analytics />
   </StrictMode>,
 )
